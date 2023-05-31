85ff39b1c33c7df58810d5cd1fcc633e258bba41
Nelson Dmonte <nelson.dmonte@gmail.com>
2023-05-31 09:52:59 +0530

    chgs

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://eoa01d6g1c.execute-api.eu-west-1.amazonaws.com",
+  baseURL: "https://hg8f031p60.execute-api.eu-west-1.amazonaws.com/",
   headers: {
     "Content-type": "application/json"
   }
