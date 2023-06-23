6565750fe7570e89f83a00e53d345febc35e1178
nelson-b <nelson.dmonte@gmail.com>
2023-06-23 10:16:26 +0530

    chgs

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const signInLink = "https://ping-sso-uat.schneider-electric.com/as/author
 +"client_id=SelloutReporting_DEV_55895&response_type=code&scope=edit&"
 +"redirect_uri=";
 
-export const redirectUrl = "http://localhost:3000/authenticate";
+export const redirectUrl = "https://master.d1txxtzp6q6is3.amplifyapp.com/authenticate";
 
 export const authorizationRequestUrl = "https://master.dje3o66qxkyld.amplifyapp.com/authendicateUser?code=";
 
